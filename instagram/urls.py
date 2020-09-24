5b57768a798192f1e7efa77e0b4aef5078205b47
KimHS0915 <remember2552@gmail.com>
2020-09-24 21:49:03 +0900

    ViewSet, Router, and Renderer

--- instagram/urls.py
+++ instagram/urls.py
@@ -8,6 +8,7 @@ router = DefaultRouter()
 router.register('post', views.PostViewSet)
 
 urlpatterns = [
-    path('public/', views.public_post_list),
+    path('mypost/<int:pk>/', views.PostDetailAPIView.as_view()),
+    # path('public/', views.public_post_list),
     path('', include(router.urls)),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -23,13 +24,40 @@ from .models import Post
 # public_post_list = PublicPostListAPIView.as_view()
 
 
-@api_view(['GET'])
-def public_post_list(request):
-    qs = Post.objects.filter(is_public=True)
-    serializer = PostSerializer(qs, many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def public_post_list(request):
+#     qs = Post.objects.filter(is_public=True)
+#     serializer = PostSerializer(qs, many=True)
+#     return Response(serializer.data)
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    @action(detail=False, methods=['GET'])
+    def public(self, request):
+        qs = self.get_queryset().filter(is_public=True)
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['PATCH'])
+    def set_public(self, request, pk):
+        instance = self.get_object()
+        instance.is_public = True
+        instance.save(update_fields=['is_public'])
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+class PostDetailAPIView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'instagram/post_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        post = self.get_object()
+        return Response({
+            # 'post': PostSerializer(post).data,
+            'post': post
+        })
